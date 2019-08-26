ccb586fca870e267a911223623a48605773a946d
Kristina Le <raendsheznon@gmail.com>
2019-08-25 23:19:52 -0500

    Release

--- app/src/main/java/com/example/fitnessapp/workout.java
+++ app/src/main/java/com/example/fitnessapp/workout.java
@@ -10,11 +10,15 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.File;
@@ -29,40 +33,25 @@ public class workout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        int workoutDay = getIntent().getIntExtra("workoutDay", 0);
-        int workoutWeek = getIntent().getIntExtra("workoutWeek", 0);
-        String[][][] weight = new String[10][7][]; // hold the weights for each exercise on each day [week][day][exercise]
+        final int workoutDay = getIntent().getIntExtra("workoutDay", 0);
+        final int workoutWeek = getIntent().getIntExtra("workoutWeek", 0);
+        final String[][][] weight = new String[10][7][]; // hold the weights for each exercise on each day [week][day][exercise]
         int[] numOfExercisesPerDay = {7, 9, 7, 8, 5, 8, 5};
-        Button reset;
-
+        int[] idStore = new int[numOfExercisesPerDay[workoutDay]];
 
         final File storageDirectory;
         storageDirectory = getFilesDir();
         final File saveData = new File(storageDirectory, "saveData.csv");
-        reset = findViewById(R.id.reset);
 
-        Log.i("workoutDay", "Contents of workoutDay are " + workoutDay);
         //Initialize jagged array size
         for (int i = 0; i < weight.length; i += 1){
-            //Toast.makeText(workout.this, "executing loop", Toast.LENGTH_SHORT).show();
             for (int j = 0; j < weight[i].length; j += 1){
                 weight[i][j] = new String[numOfExercisesPerDay[j]];
-                //Log.i("weightUpdate", "Contents of weight at " + i + j + " are " + weight[i][j]);
             }
         }
 
-        // temporary delete button
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveData.delete();
-            }
-        });
-
         // Display Banner and background image based on the day
         ImageView cl = findViewById(R.id.bgImage);
-        int currentDay = getResources().getIdentifier("day" + workoutDay + "w", "drawable", this.getPackageName());
-        Log.i("wdupdate", "Setting Background " + currentDay);
         cl.setBackgroundResource(setBackground(workoutDay));
 
 
@@ -70,10 +59,45 @@ public class workout extends AppCompatActivity {
         // load saved data
         loadData(weight, saveData);
 
-        // display the workouts based on the day and load
+        // insert the inputboxes based on day
+        for (int i = 0; i < numOfExercisesPerDay[workoutDay]; i += 1){
+            final EditText myEditText = new EditText(this);
+            float dpFactor = getResources().getDisplayMetrics().density; // this allows pixel to DP conversion
+            RelativeLayout.LayoutParams rp = new RelativeLayout.LayoutParams((int) (50 * dpFactor), (int) (50 * dpFactor));
 
-        //
+            // set the params for each num input
+            if (i == 0) {
+                rp.setMargins((int) (345 * dpFactor), (int) (250 * dpFactor), 0, 0);
+            } else {
+                rp.addRule(RelativeLayout.BELOW, idStore[i - 1]);
+                rp.setMargins((int) (345 * dpFactor), (int) (8 * dpFactor), 0, 0);
+            }
+            myEditText.setLayoutParams(rp);
 
+            // add the num inputs
+            RelativeLayout workoutLayout = findViewById(R.id.workoutLayout);
+            workoutLayout.addView(myEditText);
+
+            // generate usable ids for the numinputs
+            idStore[i] = View.generateViewId();
+            myEditText.setId(idStore[i]);
+
+            //update the boxes with data loaded from file if applicable
+            if(weight[workoutWeek][workoutDay][i] != "0"){
+                myEditText.setText(weight[workoutWeek][workoutDay][i]);
+            }
+
+            final int temp = i;
+            //set listeners to save updates when user enters weights
+            myEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus) {
+                        updateArray(workoutWeek, workoutDay, weight, temp, myEditText.getText().toString(), saveData);
+                    }
+                }
+            });
+        }
     }
 
     private void writeNewArray(String[][][] weight){
@@ -82,7 +106,6 @@ public class workout extends AppCompatActivity {
                 for (int exercise = 0; exercise < weight[week][day].length; exercise += 1) {
                     // set all array values to 0
                     weight[week][day][exercise] = "0";
-                    //Log.i("weightUpdate", "Contents of weight at " + week + day + exercise + " are " + weight[week][day][exercise]);
                 }
             }
         }
@@ -93,8 +116,10 @@ public class workout extends AppCompatActivity {
         int resID = getResources().getIdentifier(bgImgName, "drawable", this.getPackageName());
         return resID;
     }
-    private void updateArray (int week, int day, String[][][] weight, int exercise, String value){
+
+    private void updateArray (int week, int day, String[][][] weight, int exercise, String value, File saveFile){
         weight[week][day][exercise] = value;
+        saveToFile(weight, saveFile);
     }
 
     private void loadData (String[][][] weight, File saveData){
@@ -103,41 +128,34 @@ public class workout extends AppCompatActivity {
                 saveData.createNewFile();
                 // create empty weight sheet
                 writeNewArray(weight);
-                //printTest(weight);
+                saveToFile(weight, saveData);
             } catch (IOException e) {
                 Log.e("IOException", "Unable to create save file");
             }
         } else { // if the file already exists, load any saved data in it.
             try (Scanner scanner = new Scanner(saveData)){
-                Log.i("test", "Loading Saved File");
                 String row;
                 for (int week = 0; week < 10; week += 1) {
                     for (int day = 0; day < 7; day += 1) {
                         if(scanner.hasNextLine()){
                             row = scanner.nextLine();
                             weight[week][day] = row.split(",");
-                            //printTest(weight);
-                            //Log.i("weightUpdate", "Contents of weight at " + week + day + exercise + " are " + weight[week][day][exercise]);
-                            //exercise += 1;
                         }
                     }
                 }
             } catch (IOException e) {
-                Log.e("IOException", "Unable to create File");
+                Log.e("IOException", "Unable to read file");
             }
         }
-        saveToFile(weight, saveData);
     }
 
     private void saveToFile(String[][][] weight, File saveFile){
         FileWriter writer;
-        Log.i("Save", "Saving to File");
         // save the weight array to csv file
         try {
             writer = new FileWriter(saveFile); // this will overwrite any file with same name
             for(int week = 0; week < weight.length; week += 1){
                 for (int day = 0; day < weight[week].length; day += 1){
-                    Log.i("savetest", "string being appended at " + week + day + " is " + String.join(",", weight[week][day]));
                     writer.append(String.join(",", weight[week][day]));
                     writer.append("\n");
                 }
@@ -145,28 +163,8 @@ public class workout extends AppCompatActivity {
             writer.flush();
             writer.close();
         } catch (IOException e) {
-            Log.e("IOException", "Unable to write to File");
+            Log.e("IOException", "Unable to write to file");
         }
-    }
-
-    // Test function to determine if load and save work correctly
-    private void printTest(String[][][] weight){
-        Log.i("test", "Testing Print");
-        for (int week = 0; week < 10; week += 1) {
-            for (int day = 0; day < 7; day += 1) {
-                for (int exercise = 0; exercise < weight[week][day].length; exercise += 1) {
-                    // set all array values to 0
-                    Log.i("weightPrint", "printTest weight at " + week + day + exercise + " are " + weight[week][day][exercise]);
-                    //weight[week][day][exercise] = "0";
-                }
-            }
-        }
-    }
-
-    public int getHotspotColor (int hotspotId, int x, int y) {
-        ImageView img = findViewById (hotspotId);
-        Bitmap bitmap = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
-        return bitmap.getPixel(x, y);
     }
 }
 
